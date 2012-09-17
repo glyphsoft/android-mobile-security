4c7feff2a4b9c852c875e1c75bffc34a81f377bd
Mark Anro Silva <mark_anro@rocketmail.com>
2012-09-17 23:49:34 +0800

    Commit Changes

--- src/edu/project/androidmobilesecurity/ListAppActivity.java
+++ src/edu/project/androidmobilesecurity/ListAppActivity.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.ListActivity;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -24,8 +27,17 @@ public class ListAppActivity extends ListActivity {
 
 		List<ResolveInfo> list = pm.queryIntentActivities(intent, PackageManager.PERMISSION_GRANTED);
 		
+//		pm.setApplicationEnabledSetting("com.example.kaye", PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+		
+		DevicePolicyManager  mDPM =  (DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE);
+//		ComponentName mDeviceAdminSample = new ComponentName("com.example.kaye", "MainActiviy");
+//		long timeMs = 1000L*Long.parseLong("5");
+//		mDPM.setMaximumTimeToLock(mDeviceAdminSample, timeMs);
+		mDPM.lockNow();
+		
 		for (ResolveInfo rInfo : list) {
-			results.add(rInfo.activityInfo.applicationInfo.loadLabel(pm).toString());
+			//results.add(rInfo.activityInfo.applicationInfo.loadLabel(pm).toString());
+			results.add(rInfo.activityInfo.applicationInfo.packageName.toString());
 		}
 		
 		setListAdapter(new AppArrayAdapter(this, results.toArray(new String[results.size()])));
